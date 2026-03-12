21f823f7b11d5d6261c1c37db34c670ae11f480d
unknown <harshini.t577@gmail.com>
2026-03-12 19:58:18 +0530

    UC1: Print OOPS to console

---
+++ OOPSBannerUC1.java
@@ -0,0 +1,16 @@
+/**
+ * OOPSBannerUC1
+ *
+ * This application prints the text "OOPS" to the console.
+ *
+ * @author Harshini
+ * @version 1.0
+ */
+
+public class OOPSBannerUC1 {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
